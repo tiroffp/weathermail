161296db792037ce48f0ed8b6560c989a95c83e2
tiroffp@gmail.com <tiroff.p@husky.neu.edu>
2016-11-10 21:22:28 -0500

    root url resolving to dummy view

--- signup/tests.py
+++ signup/tests.py
@@ -1,8 +1,11 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from signup.views import home_page
 
 
 # Create your tests here.
 class HomePageTest(TestCase):
-        def test_home_page_renders_home_template(self):
-            response = self.client.get('/')
-            self.assertTemplateUsed(response, 'home.html')
+
+        def test_root_url_resolves_to_home_page_view(self):
+            found = resolve('/')
+            self.assertEqual(found.func, home_page)
--- weathermail/urls.py
+++ weathermail/urls.py
@@ -12,9 +12,10 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from signup import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home_page, name='home')
 ]
